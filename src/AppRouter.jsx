37522f4ed06bc375701fa28f31892176198ee576
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-14 20:01:40 -0300

    feat: passing variable colors to the pages through ThemeProvider

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,13 +1,19 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom"
+import { ThemeProvider } from "styled-components"
 import HomePage from "./pages/home"
+import GlobalStyle from "./style/GlobalStyle"
+import { theme } from "./style/Theme"
 
 const AppRouter = ()=> {
     return(
-        <BrowserRouter>
-            <Routes>
-                <Route element={<HomePage />} path={'/'} />
-            </Routes>
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <Routes>
+                    <Route element={<HomePage />} path={'/'} />
+                </Routes>
+                <GlobalStyle />
+            </BrowserRouter>
+        </ThemeProvider>
     )
 }
 
