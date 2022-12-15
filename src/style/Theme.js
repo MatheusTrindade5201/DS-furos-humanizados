e4a3d57f5942b25bf186a307552f2afc3102bb50
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-14 22:17:44 -0300

    refactor: creating a variable for the shadow

--- src/style/Theme.js
+++ src/style/Theme.js
@@ -1,6 +1,7 @@
 export const theme = {
     colors:{
         primary: '#F9F9F9',
-        secondary: '#F794CD'
+        secondary: '#F794CD',
+        shadow: '0 2px 4px 1px rgb(64, 51, 58, 0.2)'
     }
 }
