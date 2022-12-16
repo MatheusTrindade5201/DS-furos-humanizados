fbd7a67e7332bb366b87018cb322d2f46d845ce7
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-16 19:39:09 -0300

    refactor: changing base color

--- src/style/Theme.js
+++ src/style/Theme.js
@@ -1,7 +1,8 @@
 export const theme = {
     colors:{
-        primary: '#F9F9F9',
+        primary: '#FFDAEF',
         secondary: '#F794CD',
-        shadow: '0 2px 4px 1px rgb(64, 51, 58, 0.2)'
+        shadow: '0 2px 4px 1px rgb(64, 51, 58, 0.2)',
+        dark_bg: '00000057'
     }
 }
