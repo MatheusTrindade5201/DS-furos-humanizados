47d2e3392c623f696eef0dd6df4098ad3bcfb2f0
MatheusTrindade5201 <matheus.trindade5201@hotmail.com>
2022-12-16 20:00:34 -0300

    fix: adding # to the hex value of the darg_bg variable

--- src/style/Theme.js
+++ src/style/Theme.js
@@ -3,6 +3,6 @@ export const theme = {
         primary: '#FFDAEF',
         secondary: '#F794CD',
         shadow: '0 2px 4px 1px rgb(64, 51, 58, 0.2)',
-        dark_bg: '00000057'
+        dark_bg: '#00000057'
     }
 }
